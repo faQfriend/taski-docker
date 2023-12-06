b76e7c74385fcb00bf260a7443136b73c13ffcd6
Рустам Ивазов <rustam@MacBook-Air-Rustam.local>
2023-12-06 21:39:14 +0300

    Test 1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,7 +13,8 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'default')
 
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOST', default='127.0.0.1, localhost').split(', ')
+ALLOWED_HOSTS = []
+
 
 
 # Application definition
